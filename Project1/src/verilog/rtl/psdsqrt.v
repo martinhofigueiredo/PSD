b371681e0410d057351acb892c79ad3786c59ba4
Martinho Figueiredo <hell51.games@gmail.com>
2022-04-25 19:17:43 +0100

    rename folders

--- Project1/src/verilog/rtl/psdsqrt.v
+++ Project1/src/verilog/rtl/psdsqrt.v
@@ -6,8 +6,26 @@ module psdsqrt(
      input [NUMBITS-1:0] xin,   // operand, unsigned integer 32 bits 
      output reg [(NUMBITS/2)-1:0] sqrt // sqrt( xin ), unsigned integer 16 bits 
        );
-  
-  
+
+
   parameter NUMBITS = 32;
+
+  module MUL1 multiplier(sqrt,sqrt, sqtestsqrt);
+
+  wire 
+  module CO1 comparator(sqtestsqrt, l);
+
+
+endmodule
+
+module multiplier(input A, input B, output O);
+
+  assign O = A * B;
+
+endmodule
+
+module comparator(input A, input B, output O);
+
+  assign O = A >= B;
 
 endmodule
