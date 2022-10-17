28592dad68eed7e207441f95e23f8236b2d34923
Martinho Figueiredo <up201506179@fe.up.pt>
2022-10-17 11:51:35 +0100

    Added FSM for divider and comments

--- 2022/Lab2/src/verilog/rtl/psddivide.v
+++ 2022/Lab2/src/verilog/rtl/psddivide.v
@@ -4,40 +4,41 @@
 // Martinho Figueiredo e Pedro Cruz
 //-------------------------------------------------------------------------------
 
-module psddivide(
+module psddivide #(parameter NBITS = 32)
+				(
 					input         clock,		//master clock
 					input         reset,		//synch reset, active high
 					input         start,		//start a new division
 					input         stop,			//load output registers
-					input  [31:0] dividend,		// dividend
-					input  [31:0] divisor,		// divisor
-					output reg [31:0] quotient,		// quotient
-					output reg [31:0] rest			//rest
+					input  [NBITS-1:0] dividend,		// dividend
+					input  [NBITS-1:0] divisor,		// divisor
+					output reg [NBITS-1:0] quotient,		// quotient
+					output reg [NBITS-1:0] rest			//rest
 				);
 
-reg [63:0] rdiv;
-reg [31:0] rdivisor;
-wire [31:0] prest;
-wire [32:0] loadleft;
-wire [32:0] loadright;
+reg [2*NBITS-1:0] rdiv;
+reg [NBITS-1:0] rdivisor;
+wire [NBITS-1:0] prest;
+wire [NBITS:0] loadleft;
+wire [NBITS:0] loadright;
 
-assign prest[31:0] = rdiv[63:31]  - {1'b0,rdivisor};
-assign loadleft[32:0] = start ? {32'd0,dividend[32]} : (prest[32] ? rdiv[62:30] : {prest[31:0],rdiv[30]}) ;
-assign loadright[30:0] = start ? dividend[30:0] : {rdiv[29:0],~prest[32]};
+assign prest[NBITS-1:0] = rdiv[2*NBITS-1:NBITS-1]  - {1'b0,rdivisor};
+assign loadleft[NBITS:0] = start ? {32'd0,dividend[NBITS]} : (prest[NBITS] ? rdiv[2*NBITS-2:NBITS-2] : {prest[NBITS-1:0],rdiv[NBITS-2]}) ;
+assign loadright[NBITS-2:0] = start ? dividend[NBITS-2:0] : {rdiv[NBITS-3:0],~prest[NBITS]};
 
 always @(posedge clock or posedge reset) begin
 	if (reset) begin
-	  rdiv <= 64'b0;
-	  quotient <=32'b0;
-	  rest <= 32'b0;
-	  rdivisor <= 32'b0;
+	  rdiv <= 0;
+	  quotient <=0;
+	  rest <= 0;
+	  rdivisor <= 0;
 	end
 	else begin
-		rdivisor <= start ? divisor[31:0] : 32'b0; 
+		rdivisor <= start ? divisor[NBITS-1:0] : 0; 
 		rdiv[30:0] <= loadright[30:0];
-		rdiv[63:31] <= loadleft[32:0];
-		rest <= stop ? rdiv[63:31] : 32'b0;
-		quotient <= stop ? rdiv[31:0] : 32'b0;
+		rdiv[2*NBITS-1:NBITS-1] <= loadleft[NBITS:0];
+		rest <= stop ? rdiv[2*NBITS-1:NBITS-1] : 0;
+		quotient <= stop ? rdiv[NBITS-1:0] : 0;
 	end
 end
 endmodule
---
+++ 2022/Lab2/src/verilog/rtl/psddivide_fsm.v
@@ -0,0 +1,66 @@
+/* Finite State Machine to Control the divider
+# TIMING DIAGRAM
+                                            1     2     3     4    33    34    35    
+            _________                    |__   |__   |__   |     |__   |__   |__   |__  
+           |         |           clock __|  |__|  |__|  |__| ... |  |__|  |__|  |__|  |__
+    run ->-|         |->- busy           | ____|     |     |     |     |     |     |
+           |         |           run   __|/    |\____|_____| ... |_____|_____|_____|_____
+  clock ->-|         |->- start          | ____|     |     |     |     |     |     |
+           |         |           start __|/    |\____|_____| ... |_____|_____|_____|_____
+  reset ->-|         |->- stop           |     |     |     |     |     | ____|     |
+           |_________|           stop  __|_____|_____|_____| ... |_____|/    |\____|_____
+                                         |     | ____|_____|     |_____|_____|     |
+                                 stop  __|_____|/    |     | ... |     |     |\____|_____
+                                         |     |     |     |     |     |     |     |
+
+# STATE DIAGRAM
+        ______________________________________    
+       |              __                      |
+       |             |  |                     |
+    ___v___        __|__v_        _______     |      
+   |       |      |       |      |       |    |      
+|->|   0   |----->|   1   |----->|   2   |----|  
+|  |_______|      |_______|      |_______|           
+|    IDLE |        | WORK         | DONE                
+|         |        |              |
+|         |        |              |
+|     ____v__      |              |
+|    |       |<----|              |
+|----|   3   |                    |
+     |_______|<-------------------|
+       RESET   
+
+From the diagram we can see that we will have 4 states, meaning we will need 2 bits to enconde all states
+    - IDLE - 2'b0 - This state will be the normal state of the circuit after and before performing division. It can be succeded only by WORK or RESET.
+        - OUTPUTS - busy  <= 1'b0;
+                  - start <= 1'b0;
+                  - stop  <= 1'b0;
+    - WORK - 2'b1 - This state will start as soon as the positive edge of the run signal is detected.
+                    It runs for NBITS cycles to allow the number to slide on the rdiv register and be divide, and then adds to cycle to flush the memory from the rdiv to the output registers
+                    The Busy flag waits for the for the next cycle after the start flag and last for NBITS+2 cycles
+        - OUTPUTS - busy  <= 1'b1; -> for NBITS+2 cycles
+                  - start <= 1'b1; -> for 1 cycle
+                  - stop  <= 1'b0;
+    - DONE - 2'b2 - This state will push the calculated value from the rdiv register to the output registers. Due to the datapath it takes 2 cycles for the value to show up on the output net
+                    On the last cycle the Flag stop must be raised to allow the loading
+        - OUTPUTS - busy  <= 1'b0; 
+                  - start <= 1'b0; 
+                  - stop  <= 1'b1; -> Last cycle
+
+Since this controller is supposed to be for a parameterized divider there the need to implement a counter for the number of cycles based on the number of bits that the input has.
+Meaning this controller should run for 1(activation)+NBITS+2(flush result) cycles.
+
+
+
+*/
+
+module moduleName (
+    input clock,
+    input reset,
+    input run,
+    output busy,
+    output start,
+    output stop,
+);
+    
+endmodule
