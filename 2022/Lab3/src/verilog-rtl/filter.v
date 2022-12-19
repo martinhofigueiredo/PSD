3669e0ba0c3bf306b23b2446db0da0ab5f35268c
Martinho Figueiredo <up201506179@fe.up.pt>
2022-12-19 23:32:03 +0000

    changes because its doing things

--- 2022/Lab3/src/verilog-rtl/filter.v
+++ 2022/Lab3/src/verilog-rtl/filter.v
@@ -32,11 +32,12 @@ module filter(
 );
 
 parameter NWINDOW = 128;
-parameter INIT = 0;
-parameter READ = 1;
-parameter WORK = 2;
-parameter DONE = 3;
-parameter RESET = 4;
+
+parameter RSET = 0;
+parameter INIT = 1;
+parameter READ = 2;
+parameter WORK = 3;
+parameter DONE = 4;
 
 reg signed [15:0] x [NWINDOW-1:0]; //Array 128 elementos de 16 bits
 reg [2:0] state = INIT;
@@ -59,11 +60,11 @@ reg signed [15:0] odd_sample;
 integer i;// Used in for loop
 
 always @(posedge clock) begin
-    if( reset ) state = RESET;
+    if( reset ) state = RSET;
     odd_sample<=odd_network; //Loard new sample
     even_sample<=even_network; //
     case ( state )
-        RESET: begin //WIPE EVERYTHING
+        RSET: begin //WIPE EVERYTHING
             dataout <= 0; // Initialize the output register at zero
             accum = 42'd0;
             for( i = 0; i < NWINDOW; i = i + 1)
@@ -73,6 +74,7 @@ always @(posedge clock) begin
             address = 0;
             odd_sample = 16'd0;
             even_sample <= 16'd0;
+            state = INIT;
         end
         INIT: begin
             dataout <= 0; // Initialize the output register at zero
@@ -85,13 +87,12 @@ always @(posedge clock) begin
         end
         READ: begin
             if(address == 0) begin
-                accum = 42'd0; // Clear Accumulator
                 for( i = NWINDOW-1; i >0; i = i - 1 )
                     x[i] <= x[i-1]; // Shift One Element back to make space for the new datain
                 x[0] <= datain; // load data in
             end
             else if(address == 2) begin
-                product[0] <= even_sample * $signed(coeff[17:0]);
+                product[0] <= even_sample * $signed(coeff[17:0]); 
                 product[1] <= odd_sample * $signed(coeff[35:18]);
             end
             address = address + 1;
@@ -100,31 +101,35 @@ always @(posedge clock) begin
 
         WORK: begin
             accum = accum + product[0] + product[1];
+
             product[0] <= even_sample * $signed(coeff[17:0]);
             product[1] <= odd_sample * $signed(coeff[35:18]);
+            $display("product 0 = %h", product[0]);
+            $display("product 1 = %h", product[1]);
             address = address + 1;
         end
 
         DONE: begin
             dataout <= $signed(accum[31:16]); //Load output register with result from the accumulator
+            address = 0;
         end
     endcase
 end
 
 always @(negedge clock) begin //Evaluate Transition in the negative edge of the clock
-    if( reset ) state = INIT;
+    if( reset ) state = RSET;
     case ( state )
         INIT: begin
             state = din_enable ? READ : INIT;
         end
         READ: begin
-            state = ( address == 2 ) ? WORK : READ;
+            state = ( address <= 2 ) ? WORK : READ;
         end
         WORK: begin
-            state = ( address <= 65 ) ? DONE : WORK; 
+            state = ( address > NWINDOW/2 ) ? DONE : WORK; 
         end
         DONE: begin
-            state = din_enable ? READ : INIT;
+            state = din_enable ? READ : DONE;
         end
     endcase
 end
