6eda766d2fc0f0deac03e0daaf48de552666e377
Martinho Figueiredo <up201506179@fe.up.pt>
2022-12-20 00:24:54 +0000

    deliverable.zip

---
+++ 2022/Lab3/deliverable/src/verilog-rtl/filter.v
@@ -0,0 +1,124 @@
+/*
+# STATE DIAGRAM
+        _______________________________________________________    
+       |                                                       |
+       |                                                       |
+    ___v___        _______        _______          _______     |      
+   |       |      |       |      |       |        |       |    |      
+|->|   0   |----->|   1   |----->|   2   |------->|   3   |----|  
+|  |_______|      |_______|      |_______|        |_______|           
+|    INIT |        | READ         | WORK            | DONE                
+|         |        |              |                 |
+|         |        |              |                 |
+|     ____v__      |              |                 |
+|    |       |<----|              |                 |
+|----|   4   |                    |                 |
+     |_______|<-------------------|-----------------|
+       RESET   
+
+INIT -- Clear All
+*/
+
+`timescale 1ns/1ps
+
+module filter(
+    input   clock,
+    input   reset,
+    input   din_enable,
+    input signed [15:0] datain,
+    input signed [35:0] coeff,
+    output  [5:0] coeffaddress,
+    output reg signed [15:0] dataout 
+);
+
+parameter NWINDOW = 128;
+
+parameter INIT = 0;
+parameter READ = 1;
+parameter WORK = 2;
+parameter DONE = 3;
+
+reg signed [15:0] x [NWINDOW-1:0]; //Array 128 elementos de 16 bits
+reg [1:0] state = INIT;
+
+reg [6:0] address = 0; //Register to store the address for the coeff
+assign coeffaddress = address[5:0];
+
+reg signed [41:0] accum = 0; // Holds the sum of the multiplications from the last iteration
+reg signed [33:0] product [1:0] ; // Holds the multiplication between the sample and coeff
+
+wire signed [15:0] even_network;
+assign even_network = x[(address << 1)]; 
+reg signed [15:0] even_sample;
+
+wire signed [15:0] odd_network;
+assign odd_network = x[(address << 1)+1]; 
+reg signed [15:0] odd_sample;
+
+
+integer i;// Used in for loop
+
+always @(posedge clock) begin
+    if( reset ) state = RSET;
+    odd_sample<=odd_network; //Loard new sample
+    even_sample<=even_network; //
+    case ( state )
+        INIT: begin
+            dataout <= 0; // Initialize the output register at zero
+            accum = 42'd0;
+            for( i = 0; i < NWINDOW; i = i + 1)
+                x[i] <= 16'd0; // Initialize the X sample vector with zeros
+            product[1] <= 34'd0;
+            product[0] <= 34'd0;
+            address = 0;
+        end
+        READ: begin
+            if(address == 0) begin
+                for( i = NWINDOW-1; i >0; i = i - 1 )
+                    x[i] <= x[i-1]; // Shift One Element back to make space for the new datain
+                x[0] <= datain; // load data in
+            end
+            else if(address == 2) begin
+                product[0] <= even_sample * $signed(coeff[17:0]); 
+                product[1] <= odd_sample * $signed(coeff[35:18]);
+            end
+            address = address + 1;
+
+        end
+
+        WORK: begin
+            accum = accum + product[0] + product[1];
+
+            product[0] <= even_sample * $signed(coeff[17:0]);
+            product[1] <= odd_sample * $signed(coeff[35:18]);
+            //$display("product 0 = %h", product[0]);
+            //$display("product 1 = %h", product[1]);
+            address = address + 1;
+        end
+
+        DONE: begin
+            dataout <= $signed(accum[31:16]); //Load output register with result from the accumulator
+            address = 0;
+        end
+    endcase
+end
+
+always @(negedge clock) begin //Evaluate Transition in the negative edge of the clock
+    if( reset ) state = INIT;
+    case ( state )
+        INIT: begin
+            state = din_enable ? READ : INIT;
+        end
+        READ: begin
+            state = ( address <= 2 ) ? WORK : READ;
+        end
+        WORK: begin
+            state = ( address > NWINDOW/2 ) ? DONE : WORK; 
+        end
+        DONE: begin
+            state = din_enable ? READ : DONE;
+        end
+    endcase
+end
+
+endmodule
---
+++ 2022/Lab3/deliverable/src/verilog-rtl/filterbank.v
@@ -0,0 +1,124 @@
+/*
+ Filter bank toplevel module
+ 
+jca@fe.up.pt, Nov 2022
+
+	This Verilog code is property of University of Porto
+	Its utilization beyond the scope of the course Digital Systems Design
+	(Projeto de Sistemas Digitais) of the Master in Electrical 
+	and Computer Engineering requires explicit authorization from the author.
+ 
+ Implementamos um modulo filter que instanciamos o mesmo bloco 8 vezes neste modulo
+ apenas o primeiro gera os coeficientes pois todos os outros tem a mesma ligação por isso produziriam o mesmo resultado
+ -Martinho Figueiredo Pedro Cruz
+
+
+
+
+
+
+
+
+
+*/
+
+
+`timescale 1ns/1ps
+
+
+module profir(
+	input 			clock,		// Master 250 MHz clock, active in the rising edge
+	input 			reset,		// Master reset, synchronous, active high
+	input signed  [15:0] 	datain,		// Input signal sample
+	input			din_enable,	// When 1, a new sample is present at datain input (lasts 1 clock)
+	output         [5:0] 	coeffaddress,	// Address to read all the coefficient memories
+	input signed  [35:0]	coeff0,		// Coefficient read data for filter 0
+	input signed  [35:0]	coeff1,		// Coefficient read data for filter 1
+	input signed  [35:0]	coeff2,		// Coefficient read data for filter 2
+	input signed  [35:0]	coeff3,		// Coefficient read data for filter 3
+	input signed  [35:0]	coeff4,		// Coefficient read data for filter 4
+	input signed  [35:0]	coeff5,		// Coefficient read data for filter 5
+	input signed  [35:0]	coeff6,		// Coefficient read data for filter 6
+	input signed  [35:0]	coeff7,		// Coefficient read data for filter 7
+	output signed [15:0]  dataout0,	// Output data of filter 0
+	output signed [15:0]  dataout1,	// Output data of filter 1
+	output signed [15:0]  dataout2,	// Output data of filter 2
+	output signed [15:0]  dataout3,	// Output data of filter 3
+	output signed [15:0]  dataout4,	// Output data of filter 4
+	output signed [15:0]  dataout5,	// Output data of filter 5
+	output signed [15:0]  dataout6,	// Output data of filter 6
+	output signed [15:0]  dataout7	// Output data of filter 7
+);
+filter filter_0(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff0),
+	.coeffaddress(coeffaddress),
+	.dataout(dataout0) 
+);
+
+filter filter_1(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff1),
+	.dataout(dataout1) 
+);
+
+filter filter_2(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff2),
+	.dataout(dataout2) 
+);
+
+filter filter_3(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff3),
+	.dataout(dataout3) 
+);
+
+filter filter_4(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff4),
+	.dataout(dataout4) 
+);
+
+filter filter_5(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff5),
+	.dataout(dataout5) 
+);
+
+filter filter_6(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff6),
+	.dataout(dataout6) 
+);
+
+filter filter_7(
+	.clock(clock),
+	.reset(reset),
+	.din_enable(din_enable),
+	.datain(datain),
+	.coeff(coeff7),
+	.dataout(dataout7) 
+);
+endmodule
